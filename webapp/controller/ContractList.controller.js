ea6f28e59a1a7ec2e8a5dbc80d15c686fc541c6c
Loshmi Gangireddy <loshmi.gangireddy.ext@sodexo.com>
2026-08-11 13:19:59 +0000

    Updated E-billing App3 UI

--- webapp/controller/ContractList.controller.js
+++ webapp/controller/ContractList.controller.js
@@ -10,40 +10,28 @@ sap.ui.define([
         onInit: function () {
             var oModel = new JSONModel();
 
-            oModel.loadData(
-                sap.ui.require.toUrl("com/sodexo/zo2crep044/model/mockData.json"),
-                null,
-                true
-            );
+            this.getOwnerComponent().setModel(oModel, "appModel");
 
             oModel.attachRequestCompleted(function () {
                 this._filterContractsByCustomer();
-
-                if (!oModel.getProperty("/salesOrder")) {
-                    oModel.setProperty("/salesOrder", {
-                        isSaved: false,
-                        isConfirmed: false,
-                        showAddressDetails: true,
-                        addressToggleIcon: "sap-icon://hide"
-                    });
-                }
+                this._ensureSalesOrderDefaults();
             }.bind(this));
 
             oModel.attachRequestFailed(function () {
                 MessageToast.show("Mock data failed to load");
             });
 
-            this.getOwnerComponent().setModel(oModel, "appModel");
+            oModel.loadData(
+                sap.ui.require.toUrl("com/sodexo/zo2crep044/model/mockData.json"),
+                null,
+                true
+            );
         },
 
         onCustomerChange: function (oEvent) {
             var sSelectedCustomerId = oEvent.getSource().getSelectedKey();
             var oModel = this.getOwnerComponent().getModel("appModel");
-            var aCustomers = oModel.getProperty("/customers") || [];
-
-            var oSelectedCustomer = aCustomers.find(function (oCustomer) {
-                return oCustomer.customerId === sSelectedCustomerId;
-            });
+            var oSelectedCustomer = this._getCustomerById(sSelectedCustomerId);
 
             if (oSelectedCustomer) {
                 oModel.setProperty("/filters/soldToCustomerId", oSelectedCustomer.customerId);
@@ -54,9 +42,8 @@ sap.ui.define([
         },
 
         onContractCheckBoxSelect: function (oEvent) {
-            var oSelectedCheckBox = oEvent.getSource();
             var bSelected = oEvent.getParameter("selected");
-            var oContext = oSelectedCheckBox.getBindingContext("appModel");
+            var oContext = oEvent.getSource().getBindingContext("appModel");
 
             if (!oContext) {
                 return;
@@ -67,13 +54,8 @@ sap.ui.define([
             var aVisibleContracts = oModel.getProperty("/visibleContracts") || [];
             var aAllContracts = oModel.getProperty("/contracts") || [];
 
-            aVisibleContracts.forEach(function (oContract) {
-                oContract.selected = false;
-            });
-
-            aAllContracts.forEach(function (oContract) {
-                oContract.selected = false;
-            });
+            this._clearContractSelections(aVisibleContracts);
+            this._clearContractSelections(aAllContracts);
 
             if (bSelected) {
                 oSelectedContract.selected = true;
@@ -102,67 +84,18 @@ sap.ui.define([
 
             var oSelectedContract = oContext.getObject();
             var oModel = this.getOwnerComponent().getModel("appModel");
+            var oSelectedCustomer = this._getCustomerById(oSelectedContract.customerId);
 
             oModel.setProperty("/selectedContract", oSelectedContract);
 
-            var aCustomers = oModel.getProperty("/customers") || [];
-
-            var oSelectedCustomer = aCustomers.find(function (oCustomer) {
-                return oCustomer.customerId === oSelectedContract.customerId;
-            });
-
             if (oSelectedCustomer) {
-                oModel.setProperty("/salesOrder/billToCustomer", oSelectedCustomer.billToCustomer);
-
-                oModel.setProperty("/salesOrder/soldToAddressLine1", oSelectedCustomer.soldToAddressLine1);
-                oModel.setProperty("/salesOrder/soldToAddressLine2", oSelectedCustomer.soldToAddressLine2);
-                oModel.setProperty("/salesOrder/soldToAddressLine3", oSelectedCustomer.soldToAddressLine3);
-
-                oModel.setProperty("/salesOrder/billToAddressLine1", oSelectedCustomer.billToAddressLine1);
-                oModel.setProperty("/salesOrder/billToAddressLine2", oSelectedCustomer.billToAddressLine2);
-                oModel.setProperty("/salesOrder/billToAddressLine3", oSelectedCustomer.billToAddressLine3);
-
-                var aShipToOptions = oSelectedCustomer.shipToOptions || [];
-                oModel.setProperty("/shipToOptions", aShipToOptions);
-
-                if (aShipToOptions.length > 0) {
-                    var oDefaultShipTo = aShipToOptions[0];
-
-                    oModel.setProperty("/salesOrder/selectedShipToId", oDefaultShipTo.shipToId);
-                    oModel.setProperty("/salesOrder/shipToAddressLine1", oDefaultShipTo.shipToAddressLine1);
-                    oModel.setProperty("/salesOrder/shipToAddressLine2", oDefaultShipTo.shipToAddressLine2);
-                    oModel.setProperty("/salesOrder/shipToAddressLine3", oDefaultShipTo.shipToAddressLine3);
-                } else {
-                    oModel.setProperty("/salesOrder/selectedShipToId", "");
-                    oModel.setProperty("/salesOrder/shipToAddressLine1", "");
-                    oModel.setProperty("/salesOrder/shipToAddressLine2", "");
-                    oModel.setProperty("/salesOrder/shipToAddressLine3", "");
-                }
+                this._applyCustomerDetailsToSalesOrder(oSelectedCustomer);
             } else {
-                oModel.setProperty("/shipToOptions", []);
-
-                oModel.setProperty("/salesOrder/billToCustomer", "");
-
-                oModel.setProperty("/salesOrder/soldToAddressLine1", "");
-                oModel.setProperty("/salesOrder/soldToAddressLine2", "");
-                oModel.setProperty("/salesOrder/soldToAddressLine3", "");
-
-                oModel.setProperty("/salesOrder/billToAddressLine1", "");
-                oModel.setProperty("/salesOrder/billToAddressLine2", "");
-                oModel.setProperty("/salesOrder/billToAddressLine3", "");
-
-                oModel.setProperty("/salesOrder/selectedShipToId", "");
-                oModel.setProperty("/salesOrder/shipToAddressLine1", "");
-                oModel.setProperty("/salesOrder/shipToAddressLine2", "");
-                oModel.setProperty("/salesOrder/shipToAddressLine3", "");
+                this._clearCustomerDetailsFromSalesOrder();
             }
 
-            oModel.setProperty("/salesOrder/customerPoNumber", oSelectedContract.poNumber);
-            oModel.setProperty("/salesOrder/isSaved", false);
-            oModel.setProperty("/salesOrder/isConfirmed", false);
-
-            oModel.setProperty("/salesOrder/showAddressDetails", true);
-            oModel.setProperty("/salesOrder/addressToggleIcon", "sap-icon://hide");
+            this._applySelectedContractToSalesOrder(oSelectedContract);
+            this._resetSalesOrderStatus();
 
             oModel.refresh(true);
 
@@ -184,7 +117,110 @@ sap.ui.define([
             var aVisibleContracts = aContracts.filter(function (oContract) {
                 return oContract.customerId === sSelectedCustomerId;
             });
+
             oModel.setProperty("/visibleContracts", aVisibleContracts);
+        },
+
+        _ensureSalesOrderDefaults: function () {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+
+            if (!oModel.getProperty("/salesOrder")) {
+                oModel.setProperty("/salesOrder", {
+                    isSaved: false,
+                    isConfirmed: false,
+                    showAddressDetails: true,
+                    addressToggleIcon: "sap-icon://hide"
+                });
+            }
+        },
+
+        _getCustomerById: function (sCustomerId) {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+            var aCustomers = oModel.getProperty("/customers") || [];
+
+            return aCustomers.find(function (oCustomer) {
+                return oCustomer.customerId === sCustomerId;
+            });
+        },
+
+        _clearContractSelections: function (aContracts) {
+            aContracts.forEach(function (oContract) {
+                oContract.selected = false;
+            });
+        },
+
+        _applyCustomerDetailsToSalesOrder: function (oCustomer) {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+            var aShipToOptions = oCustomer.shipToOptions || [];
+
+            oModel.setProperty("/salesOrder/billToCustomer", oCustomer.billToCustomer);
+
+            oModel.setProperty("/salesOrder/soldToAddressLine1", oCustomer.soldToAddressLine1);
+            oModel.setProperty("/salesOrder/soldToAddressLine2", oCustomer.soldToAddressLine2);
+            oModel.setProperty("/salesOrder/soldToAddressLine3", oCustomer.soldToAddressLine3);
+
+            oModel.setProperty("/salesOrder/billToAddressLine1", oCustomer.billToAddressLine1);
+            oModel.setProperty("/salesOrder/billToAddressLine2", oCustomer.billToAddressLine2);
+            oModel.setProperty("/salesOrder/billToAddressLine3", oCustomer.billToAddressLine3);
+
+            oModel.setProperty("/shipToOptions", aShipToOptions);
+
+            if (aShipToOptions.length > 0) {
+                this._applyShipToDetailsToSalesOrder(aShipToOptions[0]);
+            } else {
+                this._clearShipToDetailsFromSalesOrder();
+            }
+        },
+
+        _applyShipToDetailsToSalesOrder: function (oShipTo) {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+
+            oModel.setProperty("/salesOrder/selectedShipToId", oShipTo.shipToId);
+            oModel.setProperty("/salesOrder/shipToAddressLine1", oShipTo.shipToAddressLine1);
+            oModel.setProperty("/salesOrder/shipToAddressLine2", oShipTo.shipToAddressLine2);
+            oModel.setProperty("/salesOrder/shipToAddressLine3", oShipTo.shipToAddressLine3);
+        },
+
+        _clearCustomerDetailsFromSalesOrder: function () {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+
+            oModel.setProperty("/shipToOptions", []);
+            oModel.setProperty("/salesOrder/billToCustomer", "");
+
+            oModel.setProperty("/salesOrder/soldToAddressLine1", "");
+            oModel.setProperty("/salesOrder/soldToAddressLine2", "");
+            oModel.setProperty("/salesOrder/soldToAddressLine3", "");
+
+            oModel.setProperty("/salesOrder/billToAddressLine1", "");
+            oModel.setProperty("/salesOrder/billToAddressLine2", "");
+            oModel.setProperty("/salesOrder/billToAddressLine3", "");
+
+            this._clearShipToDetailsFromSalesOrder();
+        },
+
+        _clearShipToDetailsFromSalesOrder: function () {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+
+            oModel.setProperty("/salesOrder/selectedShipToId", "");
+            oModel.setProperty("/salesOrder/shipToAddressLine1", "");
+            oModel.setProperty("/salesOrder/shipToAddressLine2", "");
+            oModel.setProperty("/salesOrder/shipToAddressLine3", "");
+        },
+
+        _applySelectedContractToSalesOrder: function (oSelectedContract) {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+
+            oModel.setProperty("/salesOrder/customerPoNumber", oSelectedContract.poNumber);
+        },
+
+        _resetSalesOrderStatus: function () {
+            var oModel = this.getOwnerComponent().getModel("appModel");
+
+            oModel.setProperty("/salesOrder/isSaved", false);
+            oModel.setProperty("/salesOrder/isConfirmed", false);
+            oModel.setProperty("/salesOrder/showAddressDetails", true);
+            oModel.setProperty("/salesOrder/addressToggleIcon", "sap-icon://hide");
         }
+
     });
 });
